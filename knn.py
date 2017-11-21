a11db806b3b3cba8e9d5681d22ba2b69f936961e
Lakshmi Manoharan <lakshmi.manoharan@hotmail.com>
2017-11-21 00:49:39 -0800

    Vectorized KLD Computation

--- knn.py
+++ knn.py
@@ -1,23 +1,37 @@
 import numpy as np
 import os
+import matplotlib.pyplot as plt
+from scipy.stats import mode
 from numpy.linalg import det, inv
+from numpy.core.umath_tests import inner1d
+from sklearn.metrics import confusion_matrix
+
+#debug
+SEP = '*****************************************\n\n***************************************'
 
 # user-defined constants
 DIM = 20
-K_VALUE = 5
+GENRES = np.array([1,4,5,9], dtype = np.int)
+KVALUES = 198
+K_START = 3
+K_END = 200
 FEATURE_DIM = 15
 
 DATASET_SIZE = 1000
 TRACK_COUNT_PER_GENRE = 100
+CURRENT_DATASET_SIZE = len(GENRES) * TRACK_COUNT_PER_GENRE
 TRAINSET_PERCENT = 0.7
 DEVSET_PERCENT = 0.15
 TESTSET_PERCENT = 0.15
 
-DEV_SET = DEVSET_PERCENT * DATASET_SIZE
-TEST_SET = TESTSET_PERCENT * DATASET_SIZE
-TRAIN_SET = TRAINSET_PERCENT * DATASET_SIZE
+DEV_SET = int(DEVSET_PERCENT * CURRENT_DATASET_SIZE)
+TRAIN_SET = int(TRAINSET_PERCENT * CURRENT_DATASET_SIZE)
+TEST_SET = int(TESTSET_PERCENT * CURRENT_DATASET_SIZE)
+
 
 FILEPATH_DATA = 'C:\\Users\\Lakshmi\Desktop\\repo229\\rock-or-not\\data'
+FILEPATH_PLOTS = 'C:\\Users\\Lakshmi\Desktop\\repo229\\rock-or-not\\plots'
+
 
 FILEPATH_MEAN = os.path.join(FILEPATH_DATA,'mean_{}.csv'.format(FEATURE_DIM))
 FILEPATH_COV = os.path.join(FILEPATH_DATA, 'cov_{}.csv'.format(FEATURE_DIM))
@@ -67,7 +81,6 @@ def splitData(genres):
 	trainCov = cov[idxTrain].reshape(idxTrain.shape[0] * FEATURE_DIM, FEATURE_DIM)
 	testCov = cov[idxTest].reshape(idxTest.shape[0] * FEATURE_DIM, FEATURE_DIM)
 
-
 	np.savetxt(FILEPATH_MEAN_TRAIN,mean[idxTrain],delimiter=',',newline='\n')
 	np.savetxt(FILEPATH_MEAN_TEST,mean[idxTest],delimiter=',',newline='\n')
 	
@@ -82,43 +95,102 @@ def splitData(genres):
 def KLDivergence(muP, muQ, covP, covQ):
 	iCovQ = inv(covQ)
 	diff = muP - muQ
+
+	# debug
+	# print np.log(det(covQ)/det(covP)) 
+	# print np.trace(np.matmul(iCovQ, covP)) 
+	# print np.matmul(diff.T, np.matmul(iCovQ, diff)) 
+	# print len(covP)
+
 	# len(covP) gives the number of dimensions, d
 	return np.log(det(covQ)/det(covP)) + np.trace(np.matmul(iCovQ, covP)) + np.matmul(diff.T, np.matmul(iCovQ, diff)) - len(covP)
 
+# Calculate KLD using vectorization
+# param <meanTrain>: TRAIN_SET X FEATURE_DIM
+# param <covTrain>: TRAIN_SET X FEATURE_DIM X FEATURE_DIM
+# param <meanTest>: TEST_SET X FEATURE_DIM
+# param <covTrain>: TEST_SET X FEATURE_DIM X FEATURE_DIM
+def KLD(meanTrain, covTrain, meanTest, covTest):
+	# Term1: log(det(covQ)/det(covP))
+	# Term4: TEST_SET X TRAIN_SET with values FEATURE_DIM
+
+	testSet = len(meanTest)
+	trainSet = TRAIN_SET
+
+	covP = det(covTest).reshape(testSet, 1)
+	covQ = det(covTrain).reshape(1, trainSet)
+	term1 = np.log(covQ / covP)
+
+	iCovQ = inv(covTrain)
+	covP = covTest
+	term2 = np.einsum('qik,pkj->pijq', iCovQ, covP)
+	term2 = np.einsum('piiq->pq', term2)
+
+	diff = (meanTest[:,:,None] - meanTrain.T[None,:,:]).reshape(testSet, 1, FEATURE_DIM, trainSet)
+	sub = np.einsum('pikq,qkj->pijq', diff, iCovQ)
+	term3 = np.einsum('pikq, pkiq->pq', sub, diff)
+
+	term4 = np.ones((testSet, trainSet)) * FEATURE_DIM
+
+	dist = term1 + term2 + term3 - term4
+	
+	# # debug
+	# print term1[0,0]
+	# print term2[0,0]
+	# print term3[0,0]
+	# print term4[0,0]
+	# print dist.shape
+
+	return dist
+
 def readData():
 	# Read all training data and associated labels
-	meanTrain = np.loadtxt('meanTrain_{}.csv'.format(DIM), delimiter = ',')
-	covTrain = np.loadtxt('covTrain_{}.csv'.format(DIM), delimiter = ',')
-	labels = np.loadtxt('labels.csv')
+	meanTrain = np.loadtxt(FILEPATH_MEAN_TRAIN, delimiter = ',')
+	covTrain = np.loadtxt(FILEPATH_COV_TRAIN, delimiter = ',')
 	# Reshape covariance matrix
-	covTrain = covTrain.reshape(700, DIM, DIM)
+	covTrain = covTrain.reshape(TRAIN_SET, FEATURE_DIM, FEATURE_DIM)
+	labelsTrain = np.loadtxt(FILEPATH_LABELS_TRAIN)
+
 
 	# Read all test data
-	meanTest = np.loadtxt('meanTest_{}.csv'.format(DIM), delimiter = ',')
-	covTest = np.loadtxt('covTest_{}.csv'.format(DIM), delimiter = ',')
-	labelsTest = np.loadtxt('labelsTest.csv')
-	# Reshape covariance matrix
-	covTest = covTest.reshape(300, DIM, DIM)
+	meanTest = np.loadtxt(FILEPATH_MEAN_TEST, delimiter = ',')
+	meanDev = meanTest[0:DEV_SET]
+	meanTest = meanTest[DEV_SET:(DEV_SET+TEST_SET)]
 
-	return meanTrain, covTrain, labels, meanTest, covTest, labelsTest
+	covTest = np.loadtxt(FILEPATH_COV_TEST, delimiter = ',')
+	covTest = covTest.reshape((DEV_SET+TEST_SET), FEATURE_DIM, FEATURE_DIM)
 
-def knnKLD(meanTrain, covTrain, labels, meanTest, covTest, labelsTest):
-	dist = np.zeros((DEV_SET, TRAIN_SET))
-	pred = np.zeros(DEV_SET)
-	labelsDev = labelsTest[0:DEV_SET].astype(int)
+	covDev = covTest[0:DEV_SET].copy()
+	covTest = covTest[DEV_SET:(DEV_SET+TEST_SET)]
+	covDev = covDev.reshape(DEV_SET, FEATURE_DIM, FEATURE_DIM)	
+	covTest = covTest.reshape(TEST_SET, FEATURE_DIM, FEATURE_DIM)	
 
-	accuracy = np.zeros(497)
+	labelsTest = np.loadtxt(FILEPATH_LABELS_TEST)
+	labelsDev = labelsTest[0:DEV_SET]
+	labelsTest = labelsTest[DEV_SET:(DEV_SET+TEST_SET)]
+
+	return meanTrain, covTrain, labelsTrain, meanDev, covDev, labelsDev, meanTest, covTest, labelsTest
+
+def knnKLD(meanTrain, covTrain, labelsTrain, meanTest, covTest, labelsTest):
+	testSet = len(meanTest)
+	trainSet = len(meanTrain)
+
+	dist = np.zeros((testSet, trainSet))
+	pred = np.zeros(testSet)
+	labelsDev = labelsTest[0:testSet].astype(int)
+
+	accuracy = np.zeros(18)
 	
-	for K_VALUE in xrange(3,500):
+	for K_VALUE in xrange(3,21):
 		print 'Classifying for K={}'.format(K_VALUE)
-		for x in xrange(0,DEV_SET):
+		for x in xrange(0,testSet):
 			#  Find distance of data point with every song in the training set
-			for y in xrange(0,TRAIN_SET):
+			for y in xrange(0,trainSet):
 				dist[x,y] = KLDivergence(meanTest[x], meanTrain[y], covTest[x], covTrain[y])		
 
 			# Find the k nearest songs
 			knearest = np.argsort(dist[x])[0:K_VALUE]
-			knearest = labels[knearest].astype(int)
+			knearest = labelsTrain[knearest].astype(int)
 			# Classify song with the maximum label count
 			counts = np.bincount(knearest)
 			pred[x] = np.argmax(counts)
@@ -126,20 +198,60 @@ def knnKLD(meanTrain, covTrain, labels, meanTest, covTest, labelsTest):
 
 			pred = pred.astype(int)
 			# Calculate accuracy
-			accuracy[K_VALUE-3] = np.where(labelsDev == pred)[0].shape[0] * 1. / DEV_SET
+			accuracy[K_VALUE-3] = np.where(labelsDev == pred)[0].shape[0] * 1. / testSet
 		print 'Accuracy for K={}: {}'.format(K_VALUE, accuracy[K_VALUE-3])		
 
 	print 'Best Accuracy obtained for K={}'.format(np.argmax(accuracy)+3)
+	np.savetxt(os.path.join(FILEPATH_DATA,'accuracy3to20.csv'), accuracy)
+	plt.plot(range(3,21),accuracy)
+	plt.show()
 
+# This implementation uses vectorized KLD method to determine the KL distance
+def knn(k, meanTrain, covTrain, labelsTrain, meanTest, covTest):
+	dist = KLD(meanTrain, covTrain, meanTest, covTest)
+
+	neighbours = np.argsort(dist, axis=1)[:,range(0,k)]
+	# neighbours = np.array(map(lambda x: labelsTrain[x], neighbours))
+	neighbours = labelsTrain[neighbours]
+	pred = np.array(mode(neighbours, axis=1)[0]).reshape(len(meanTest),)	
+	
+	return pred
 
 def main():
 	# Choose the following genres: Classical, HipHop, Jazz and Rock
-	genres = np.array([1,4,5,9], dtype=np.int)
-	splitData(genres)
+	splitData(GENRES)
 
 	# Fit KNN
-	# meanTrain, covTrain, labels, meanTest, covTest, labelsTest = readData()
-	# knnKLD(meanTrain, covTrain, labels, meanTest, covTest, labelsTest)
+	meanTrain, covTrain, labelsTrain, meanDev, covDev, labelsDev, meanTest, covTest, labelsTest = readData()
+	accuracy = np.zeros(KVALUES)
+
+	# Find optimal K by using the Dev-Set
+	# for k in xrange(K_START,K_END+1):				
+	# 	pred = knn(k, meanTrain, covTrain, labelsTrain, meanDev, covDev)
+	# 	accuracy[k-3] = np.where(pred == labelsDev)[0].shape[0] * 1. / len(meanDev)
+	# 	print 'k={}\tAccuracy: {}'.format(k,accuracy[k-3])
+
+	# # Plot k-Value vs Accuracy
+	# plt.figure()
+	# plt.plot(range(K_START,K_END+1), accuracy)	
+	# plt.xlabel('K Value')
+	# plt.ylabel('Accuracy')
+	# plt.title('Accuracy vs K Value for Dev-Set')
+	# plt.savefig(os.path.join(FILEPATH_PLOTS, 'kVsAccuracy.png'))
+	# plt.show()
+
+	# kOptimal = np.argsort(accuracy)[-1] + 3
+	# print '\n k-optimal: {} with accuracy = {}'.format(kOptimal, accuracy[kOptimal-3])
+
+	# Predict labels for test set using k-optimal 
+	pred = knn(kOptimal, meanTrain, covTrain, labelsTrain, meanTest, covTest)
+	accuracyTest = np.where(pred == labelsTest)[0].shape[0] * 1. / len(meanTest)
+	print pred
+	print labelsTest
+	print 'Accuracy: {}'.format(accuracyTest)
+	print 'Confusion Matrix'
+	print confusion_matrix(labelsTest, pred)
+
 
 if __name__ == '__main__':
 	main()
---
+++ knn_randomShuffle.py
@@ -0,0 +1,315 @@
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+from scipy.stats import mode
+from numpy.linalg import det, inv
+from numpy.core.umath_tests import inner1d
+from sklearn.metrics import confusion_matrix
+
+#debug
+SEP = '*****************************************\n\n***************************************'
+
+# user-defined constants
+DIM = 20
+GENRES = np.array([1,4,5,9], dtype = np.int)
+K_START = 3
+K_END = 201
+FEATURE_DIM = 15
+ITERATIONS = 10
+
+DATASET_SIZE = 1000
+TRACK_COUNT_PER_GENRE = 100
+CURRENT_DATASET_SIZE = len(GENRES) * TRACK_COUNT_PER_GENRE
+TRAINSET_PERCENT = 0.7
+DEVSET_PERCENT = 0.15
+TESTSET_PERCENT = 0.15
+
+DEV_SET = int(DEVSET_PERCENT * CURRENT_DATASET_SIZE)
+TRAIN_SET = int(TRAINSET_PERCENT * CURRENT_DATASET_SIZE)
+TEST_SET = int(TESTSET_PERCENT * CURRENT_DATASET_SIZE)
+
+
+FILEPATH_DATA = 'C:\\Users\\Lakshmi\Desktop\\repo229\\rock-or-not\\data'
+FILEPATH_PLOTS = 'C:\\Users\\Lakshmi\Desktop\\repo229\\rock-or-not\\plots'
+
+
+FILEPATH_MEAN = os.path.join(FILEPATH_DATA,'mean_{}.csv'.format(FEATURE_DIM))
+FILEPATH_COV = os.path.join(FILEPATH_DATA, 'cov_{}.csv'.format(FEATURE_DIM))
+FILEPATH_LABELS = os.path.join(FILEPATH_DATA, 'labels.csv')
+
+FILEPATH_MEAN_TRAIN = os.path.join(FILEPATH_DATA, 'meanTrain_{}.csv'.format(FEATURE_DIM))
+FILEPATH_COV_TRAIN = os.path.join(FILEPATH_DATA, 'covTrain_{}.csv'.format(FEATURE_DIM))
+FILEPATH_MEAN_DEV = os.path.join(FILEPATH_DATA, 'meanDev_{}.csv'.format(FEATURE_DIM))
+FILEPATH_COV_DEV = os.path.join(FILEPATH_DATA, 'covDev_{}.csv'.format(FEATURE_DIM))
+FILEPATH_MEAN_TEST = os.path.join(FILEPATH_DATA, 'meanTest_{}.csv'.format(FEATURE_DIM))
+FILEPATH_COV_TEST = os.path.join(FILEPATH_DATA, 'covTest_{}.csv'.format(FEATURE_DIM))
+FILEPATH_LABELS_TRAIN = os.path.join(FILEPATH_DATA, 'labelsTrain.csv')
+FILEPATH_LABELS_TEST = os.path.join(FILEPATH_DATA, 'labelsTest.csv')
+FILEPATH_LABELS_DEV = os.path.join(FILEPATH_DATA, 'labelsDev.csv')
+
+
+def printDecorated(message):
+	print '\n**********************************************'
+	print message
+	print '\n**********************************************'
+	return
+
+# Splits data into training set, dev set and test set.
+# Param <genres>: Array specifying the genres to consider
+def splitData(genres):
+	# Read all data from file
+	mean = np.loadtxt(FILEPATH_MEAN, delimiter = ',')
+	cov = np.loadtxt(FILEPATH_COV, delimiter = ',')
+	cov = cov.reshape(DATASET_SIZE, FEATURE_DIM, FEATURE_DIM)
+	labels = np.loadtxt(FILEPATH_LABELS)
+
+	# Extract records relevant to genres specified in the parameter <genres>
+	indices = []
+	for x in genres:
+		idx = range(x * TRACK_COUNT_PER_GENRE, x * TRACK_COUNT_PER_GENRE + TRACK_COUNT_PER_GENRE)
+		indices = np.append(indices, idx)
+	indices = np.array(indices, dtype = np.int)	
+	mean = mean[indices]
+	cov = cov[indices]
+	
+	# allIndex = np.array(range(0,CURRENT_DATASET_SIZE))
+	# idxTrain = np.random.choice(CURRENT_DATASET_SIZE, TRAIN_SET, replace = False)
+	# idxTest = np.setdiff1d(allIndex, idxTrain)
+
+	idxTrain = []
+	idxDev = []
+	idxTest = []
+	a = []
+	b = []	
+	c = []
+
+	# Extract train and test records and save as CSV
+	for x, g in enumerate(genres):
+		a = range(x * TRACK_COUNT_PER_GENRE, x * TRACK_COUNT_PER_GENRE + int(TRAINSET_PERCENT * TRACK_COUNT_PER_GENRE))
+		idxTrain = np.append(idxTrain,a)
+		b = range(x * TRACK_COUNT_PER_GENRE + int(TRAINSET_PERCENT * TRACK_COUNT_PER_GENRE), x * TRACK_COUNT_PER_GENRE + int((TRAINSET_PERCENT + DEVSET_PERCENT) * TRACK_COUNT_PER_GENRE))
+		idxDev = np.append(idxDev,b)
+		c = range(x * TRACK_COUNT_PER_GENRE + int( (TRAINSET_PERCENT + DEVSET_PERCENT) * TRACK_COUNT_PER_GENRE), (x+1) * TRACK_COUNT_PER_GENRE)
+		idxTest = np.append(idxTest,c)
+
+	# Extract train and test records and save as CSV
+	idxTrain = np.array(idxTrain, dtype = np.int)
+	idxDev = np.array(idxDev, dtype = np.int)
+	idxTest = np.array(idxTest, dtype = np.int)
+
+	trainCov = cov[idxTrain].reshape(idxTrain.shape[0] * FEATURE_DIM, FEATURE_DIM)
+	devCov = cov[idxDev].reshape(idxDev.shape[0] * FEATURE_DIM, FEATURE_DIM)
+	testCov = cov[idxTest].reshape(idxTest.shape[0] * FEATURE_DIM, FEATURE_DIM)
+
+	np.savetxt(FILEPATH_MEAN_TRAIN,mean[idxTrain],delimiter=',',newline='\n')
+	np.savetxt(FILEPATH_MEAN_DEV,mean[idxDev],delimiter=',',newline='\n')
+	np.savetxt(FILEPATH_MEAN_TEST,mean[idxTest],delimiter=',',newline='\n')
+	
+	np.savetxt(FILEPATH_COV_TRAIN, trainCov, delimiter=',', newline='\n')
+	np.savetxt(FILEPATH_COV_DEV, devCov, delimiter=',', newline='\n')
+	np.savetxt(FILEPATH_COV_TEST, testCov, delimiter=',', newline='\n')	
+
+	np.savetxt(FILEPATH_LABELS_TRAIN,labels[idxTrain],delimiter=',',newline='\n')
+	np.savetxt(FILEPATH_LABELS_DEV,labels[idxDev],delimiter=',',newline='\n')
+	np.savetxt(FILEPATH_LABELS_TEST,labels[idxTest],delimiter=',',newline='\n')	
+
+# Calculate KLD using vectorization
+# param <meanTrain>: TRAIN_SET X FEATURE_DIM
+# param <covTrain>: TRAIN_SET X FEATURE_DIM X FEATURE_DIM
+# param <meanTest>: TEST_SET X FEATURE_DIM
+# param <covTrain>: TEST_SET X FEATURE_DIM X FEATURE_DIM
+def KLD(meanTrain, covTrain, meanTest, covTest):
+	# Term1: log(det(covQ)/det(covP))
+	# Term4: TEST_SET X TRAIN_SET with values FEATURE_DIM
+
+	testSet = len(meanTest)
+	trainSet = TRAIN_SET
+
+	print covTest.shape
+	covP = det(covTest).reshape(testSet, 1)
+	covQ = det(covTrain).reshape(1, trainSet)
+	term1 = np.log(covQ / covP)
+
+	iCovQ = inv(covTrain)
+	covP = covTest
+	term2 = np.einsum('qik,pkj->pijq', iCovQ, covP)
+	term2 = np.einsum('piiq->pq', term2)
+
+	diff = (meanTest[:,:,None] - meanTrain.T[None,:,:]).reshape(testSet, 1, FEATURE_DIM, trainSet)
+	sub = np.einsum('pikq,qkj->pijq', diff, iCovQ)
+	term3 = np.einsum('pikq, pkiq->pq', sub, diff)
+
+	term4 = np.ones((testSet, trainSet)) * FEATURE_DIM
+
+	dist = term1 + term2 + term3 - term4
+	return dist
+
+# Calculate KL Divergence between two songs given their mean vectors and covariance matrices
+def KLDivergence(muP, muQ, covP, covQ):
+	iCovQ = inv(covQ)
+	diff = muP - muQ
+	# len(covP) gives the number of dimensions, d
+	return np.log(det(covQ)/det(covP)) + np.trace(np.matmul(iCovQ, covP)) + np.matmul(diff.T, np.matmul(iCovQ, diff)) - len(covP)
+
+def readData():
+	# Read all training data and associated labels
+	meanTrain = np.loadtxt(FILEPATH_MEAN_TRAIN, delimiter = ',')
+	covTrain = np.loadtxt(FILEPATH_COV_TRAIN, delimiter = ',')
+	# Reshape covariance matrix
+	covTrain = covTrain.reshape(TRAIN_SET, FEATURE_DIM, FEATURE_DIM)
+	labelsTrain = np.loadtxt(FILEPATH_LABELS_TRAIN)
+
+	# Read all training data and associated labels
+	meanDev = np.loadtxt(FILEPATH_MEAN_DEV, delimiter = ',')
+	covDev = np.loadtxt(FILEPATH_COV_DEV, delimiter = ',')
+	# Reshape covariance matrix
+	covDev = covDev.reshape(DEV_SET, FEATURE_DIM, FEATURE_DIM)
+	labelsDev = np.loadtxt(FILEPATH_LABELS_DEV)
+
+	# Read all training data and associated labels
+	meanTest = np.loadtxt(FILEPATH_MEAN_TEST, delimiter = ',')
+	covTest = np.loadtxt(FILEPATH_COV_TEST, delimiter = ',')
+	# Reshape covariance matrix
+	covTest = covDev.reshape(TEST_SET, FEATURE_DIM, FEATURE_DIM)
+	labelsTest = np.loadtxt(FILEPATH_LABELS_TEST)
+
+	return meanTrain, covTrain, labelsTrain, meanDev, covDev, labelsDev, meanTest, covTest, labelsTest
+
+def knnKLD(meanTrain, covTrain, labelsTrain, meanTest, covTest, labelsTest, trial):
+	testSet = len(meanTest)
+	trainSet = len(meanTrain)
+
+	dist = np.zeros((testSet, trainSet))
+	pred = np.zeros(testSet)
+	labelsDev = labelsTest[0:testSet].astype(int)
+
+	accuracy = np.zeros(18)
+	
+	for K_VALUE in xrange(3,21):
+		print 'Classifying for K={}'.format(K_VALUE)
+		for x in xrange(0,testSet):
+			#  Find distance of data point with every song in the training set
+			for y in xrange(0,trainSet):
+				dist[x,y] = KLDivergence(meanTest[x], meanTrain[y], covTest[x], covTrain[y])		
+
+			# Find the k nearest songs
+			knearest = np.argsort(dist[x])[0:K_VALUE]
+			knearest = labelsTrain[knearest].astype(int)
+			# Classify song with the maximum label count
+			counts = np.bincount(knearest)
+			pred[x] = np.argmax(counts)
+			# print 'Classifying record {} as Genre[{}]'.format(x+1, pred[x])
+
+			pred = pred.astype(int)
+			# Calculate accuracy
+			accuracy[K_VALUE-3] = np.where(labelsDev == pred)[0].shape[0] * 1. / testSet
+		print 'Accuracy for K={}: {}'.format(K_VALUE, accuracy[K_VALUE-3])		
+
+	print 'Best Accuracy: {}, \tFor K={}'.format(np.max(accuracy),np.argmax(accuracy)+3)
+	# Plot and save Accuracy-vs-K-Value
+	plt.plot(range(3,21),accuracy)
+	plt.xlabel('K Value')
+	plt.ylabel('Accuracy')
+	plt.title('Accuracy vs K Value for Dev-Set')
+	plt.savefig(os.path.join(FILEPATH_PLOTS, 'knn_acc_{}.png'.format(trial)))
+
+
+	# Plot and save Error-vs-K-Value
+	# error = np.ones(18) - accuracy
+	# plt.plot(range(3,21), error)
+	# plt.xlabel('K Value')
+	# plt.ylabel('Error in Prediction')
+	# plt.title('Accuracy vs K Value for Dev-Set')
+	# plt.savefig(os.path.join(FILEPATH_PLOTS, 'knn_error_{}.png'.format(TRIAL_NO)))
+
+	return np.max(accuracy), np.argmax(accuracy)+3
+
+def knn(k, meanTrain, covTrain, labelsTrain, meanTest, covTest):
+	dist = KLD(meanTrain, covTrain, meanTest, covTest)
+
+	neighbours = np.argsort(dist, axis=1)[:,range(0,k)]
+	# neighbours = np.array(map(lambda x: labelsTrain[x], neighbours))
+	neighbours = labelsTrain[neighbours]
+	pred = np.array(mode(neighbours, axis=1)[0]).reshape(len(meanTest),)	
+
+	return pred
+
+def main():
+	# # Fit using slow KNN
+	# accuracy = np.zeros((10,2))
+
+	# for x in xrange(0,10):
+	# 	# Choose the following genres: Classical, HipHop, Jazz and Rock
+	# 	genres = np.array([1,4,5,9], dtype=np.int)
+	# 	splitData(genres)		
+
+	# 	meanTrain, covTrain, labelsTrain, meanDev, covDev, labelsDev, meanTest, covTest, labelsTest = readData()		
+	# 	accuracy[x] = knnKLD(meanTrain, covTrain, labelsTrain, meanDev, covDev, labelsDev, x)
+
+	# np.savetxt(os.path.join(FILEPATH_PLOTS, 'acc.csv'), accuracy, delimiter=',')
+
+
+	# Training with Dev for Vectorized KNN
+	accuracy = np.zeros(K_END - K_START)
+
+	# Choose the following genres: Classical, HipHop, Jazz and Rock
+	splitData(GENRES)		
+	meanTrain, covTrain, labelsTrain, meanDev, covDev, labelsDev, meanTest, covTest, labelsTest = readData()
+
+	# Find optimal K by using the Dev-Set
+	for k in xrange(K_START, K_END):				
+		pred = knn(k, meanTrain, covTrain, labelsTrain, meanDev, covDev)
+		accuracy[k-3] = np.where(pred == labelsDev)[0].shape[0] * 1. / len(meanDev)
+		print 'k={}\tAccuracy: {}'.format(k,accuracy[k-3])
+
+	optimalK = int(np.argmax(accuracy) + 3)
+	print '\n k-optimal: {} with accuracy = {}'.format(optimalK, accuracy[optimalK])
+
+	# # Plot k-Value vs Accuracy
+	plt.figure()
+	plt.plot(range(K_START,K_END), accuracy)	
+	plt.xlabel('K Value')
+	plt.ylabel('Accuracy')
+	plt.title('Accuracy vs K Value for Dev-Set')
+	plt.savefig(os.path.join(FILEPATH_PLOTS, 'kValueVsAccuracyFinal.png'))
+
+	# # Choose the following genres: Classical, HipHop, Jazz and Rock
+	# splitData(GENRES)		
+	# accuracy = np.zeros(K_END - K_START)
+
+	# meanTrain, covTrain, labelsTrain, meanDev, covDev, labelsDev, meanTest, covTest, labelsTest = readData()
+
+	# # Find optimal K by using the Dev-Set
+	# for k in xrange(K_START, K_END):				
+	# 	pred = knn(k, meanTrain, covTrain, labelsTrain, meanDev, covDev)
+	# 	accuracy[k-3] = np.where(pred == labelsDev)[0].shape[0] * 1. / len(meanDev)
+	# 	# pred = knn(k, meanTrain, covTrain, labelsTrain, meanTest, covTest)
+	# 	# accuracy[k-3] = np.where(pred == labelsTest)[0].shape[0] * 1. / len(meanTest)
+	# 	print 'k={}\tAccuracy: {}'.format(k,accuracy[k-3])
+
+	# optimalK[x] = int(np.argmax(accuracy) + 3)
+	# print '\n k-optimal: {} with accuracy = {}'.format(optimalK[x], accuracy[int(optimalK[x]-3)])
+
+	# # # Plot k-Value vs Accuracy
+	# plt.figure()
+	# plt.plot(range(K_START,K_END), accuracy)	
+	# plt.xlabel('K Value')
+	# plt.ylabel('Accuracy')
+	# plt.title('Accuracy vs K Value for Dev-Set')
+	# plt.savefig(os.path.join(FILEPATH_PLOTS, 'kVsAccuracy{}.png'.format(x+1)))		
+
+	# print optimalK
+	# print mode(optimalK)[0]
+
+	# kValueForTest = int(np.mean(optimalK))
+	
+	# Comment out the part above and run with kValueForTest replaced by optimal K obtained from above.
+	# Predict labels for test set using k-optimal 
+	pred = knn(optimalK, meanTrain, covTrain, labelsTrain, meanTest, covTest)
+	accuracyTest = np.where(pred == labelsTest)[0].shape[0] * 1. / len(meanTest)
+	print 'Accuracy: {}'.format(accuracyTest)
+	print 'Confusion Matrix'
+	print confusion_matrix(labelsTest, pred)
+
+if __name__ == '__main__':
+	main()
